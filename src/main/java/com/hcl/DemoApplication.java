647d809ff7b488ef4ac78701c9b5fe5f7be1520b
kotaGayatriPriyanka <84893898+kotaGayatriPriyanka@users.noreply.github.com>
2021-06-01 11:06:52 +0530

    Update DemoApplication.java

--- src/main/java/com/hcl/DemoApplication.java
+++ src/main/java/com/hcl/DemoApplication.java
@@ -3,13 +3,23 @@ package com.hcl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer
+{
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context =SpringApplication.run(DemoApplication.class, args);
 		
+		
 	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(DemoApplication.class);
+    }
+
 
 }
